5d691131f460c49f00056aa309b6d475862ee132
Kory Prince <korylprince@gmail.com>
2013-07-16 18:51:50 -0500

    got graphs working

--- client/web/__init__.py
+++ client/web/__init__.py
@@ -1,12 +1,19 @@
+import sqlite3
+import os, datetime
 from flask import Flask
 from flask import render_template, jsonify, request
+import BeagleCommand
 from BeagleCommand.util import Message
 
 app = Flask(__name__)
 
+dbpath = '{0}/storage/{1}-web.db'.format(os.path.abspath(os.path.dirname(BeagleCommand.__file__)),datetime.datetime.now().strftime('%m.%d.%Y'))
+conn = sqlite3.connect(dbpath)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = getData()
+    return render_template('index.html',data=data,kw=data['kilowattData'][0][1])
 
 @app.route('/get')
 def get():
@@ -21,3 +28,17 @@ def commandPost():
     m = Message(to=['serial'], msg=[request.form['command'],[0.0]])
     app.MessageBox.put(m)
     return jsonify(status=request.form['command'])
+
+def getData():
+    voltage = list()
+    motor = list()
+    charge = list()
+    kilowatt = list()
+    for row in conn.execute('select * from data order by timestamp desc limit 60;'):
+        t = int(row[0]*1000)
+        voltage.append([t,row[1]])
+        motor.append([t,row[2]])
+        charge.append([t,row[3]])
+        kilowatt.append([t,row[4]])
+    return dict(voltageData=voltage, motorData=motor, chargeData=charge, kilowattData=kilowatt)
+
